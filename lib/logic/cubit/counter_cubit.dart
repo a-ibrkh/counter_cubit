72558fe3dba00db163278b4c22d67128d4d54b67
a-ibrkh <axmadxojaibrohimov@gmail.com>
2022-04-12 10:28:52 +0500

    audio added

--- lib/logic/cubit/counter_cubit.dart
+++ lib/logic/cubit/counter_cubit.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
@@ -6,13 +7,21 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> with HydratedMixin {
-  CounterCubit() : super(CounterState(counterValue: 0, isIncrement: false)) {}
-  void increment() => emit(
-        CounterState(counterValue: state.counterValue + 1, isIncrement: true),
-      );
+  final AudioCache audioPlayer = AudioCache();
 
-  void decrement() => emit(
-      CounterState(counterValue: state.counterValue - 1, isIncrement: false));
+  CounterCubit() : super(CounterState(counterValue: 0, isIncrement: false)) {}
+  void increment() {
+    playAudio('audio/increment.mp3');
+    emit(
+      CounterState(counterValue: state.counterValue + 1, isIncrement: true),
+    );
+  }
+
+  void decrement() {
+    playAudio('audio/decrement.mp3');
+    emit(
+        CounterState(counterValue: state.counterValue - 1, isIncrement: false));
+  }
 
   @override
   CounterState? fromJson(Map<String, dynamic> json) {
@@ -23,5 +32,9 @@ class CounterCubit extends Cubit<CounterState> with HydratedMixin {
   Map<String, dynamic>? toJson(CounterState state) {
     return state.toMap();
   }
-}
 
+  void playAudio(String url) async {
+    final result = await audioPlayer.play(url);
+    result;
+  }
+}
